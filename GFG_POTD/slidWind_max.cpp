766d064f604d5eeb41a53b34013c43ba6ac966ed
Om Patil <kushpo.030507@gmail.com>
2022-10-25 14:15:07 +0530

    Find max for every contiguous subarray of size K

---
+++ GFG_POTD/slidWind_max.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
+        vector<int> v;
+        multiset<int> s;
+        int we = k - 1, ws = 0;
+        for(int i = 0; i < k - 1; i++)
+            s.insert(arr[i]);
+        while(we < n)
+        {
+            s.insert(arr[we]);
+            v.push_back(*s.rbegin());
+            s.erase(s.find(arr[ws]));
+            we++;
+            ws++;
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    int arr[n];
+	    for(int i = 0;i<n;i++) 
+	        cin >> arr[i];
+	    Solution ob;
+	    vector <int> res = ob.max_of_subarrays(arr, n, k);
+	    for (int i = 0; i < res.size (); i++) 
+	        cout << res[i] << " ";
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
