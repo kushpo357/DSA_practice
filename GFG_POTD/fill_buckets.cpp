a468c6eb19f40fa3a8823d8f2fe5eaabb56091b1
Om Patil <kushpo.030507@gmail.com>
2022-11-13 09:38:08 +0530

    No. of ways to fill bucket with distinct no. of balls and > 1?

---
+++ GFG_POTD/fill_buckets.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int totalWays(int n, vector<int>cap) {
+        // code here
+        sort(cap.begin(), cap.end());
+        long long ans = 1;
+        
+        for(long long i = 0; i < n; i++)
+            ans = (ans*(cap[i] - i))%1000000007;
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>capacity(n);
+		for(int i = 0; i < n; i++)
+			cin >> capacity[i];
+		Solution ob;
+		int ans = ob.totalWays(n, capacity);
+		cout << ans << "\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
