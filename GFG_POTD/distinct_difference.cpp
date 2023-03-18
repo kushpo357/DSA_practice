c6b697b36df1eea7cc7191330ac949cdc040c2b6
Om Patil <kushpo.030507@gmail.com>
2023-03-19 01:07:26 +0530

    Find diff b/w num of distinct ele in it's left and right

---
+++ GFG_POTD/distinct_difference.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> getDistinctDifference(int n, vector<int> &a) {
+        // code here
+         unordered_map<int, int> mp;
+        
+        for(int i = 0; i < n; i++) mp[a[i]]++;
+        
+        unordered_map<int, int> mp1;
+        vector<int> ans;
+        for(int i = 0; i < n; i++)
+        {
+            mp[a[i]]--;
+            if(mp[a[i]] == 0) mp.erase(a[i]);
+            ans.push_back(mp1.size() - mp.size());
+            mp1[a[i]]++;
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        vector<int> res = obj.getDistinctDifference(N, A);
+        
+        Array::print(res);
+        
+    }
+}
+
+// } Driver Code Ends
