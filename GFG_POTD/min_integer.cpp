da75b25af63f3df4506606e8f763daf9cc0b7ef9
Om Patil <kushpo.030507@gmail.com>
2023-03-30 10:28:13 +0530

    Find min element ; (sum of array) <= (num of elements)*element

---
+++ GFG_POTD/min_integer.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimumInteger(int n, vector<int> &a) {
+        // code here
+        long long sum = 0;
+        
+        for(int i = 0; i < n; i++) sum += a[i];
+        
+        int ans = INT_MAX;
+        
+        for(int i = 0; i < n; i++)
+            if(sum <= (long long)n*a[i]) ans = min(ans, a[i]);
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.minimumInteger(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
