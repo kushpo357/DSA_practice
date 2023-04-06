c198ecd7861fcb46326bb5da71d299814725e9a7
Om Patil <kushpo.030507@gmail.com>
2023-04-06 23:14:39 +0530

    Find the position where right subarray sum = left subarray sum

---
+++ GFG_POTD/leftRightSubarraySum.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int equalSum(int n, vector<int> &a) {
+        // code here
+        
+        if(n == 1) return 1;
+        
+        long long int sum = 0, leftsum = 0;
+        
+        for(int i = 0; i < n; i++) sum += a[i];
+        
+        for(int i = 1; i < n; i++)
+        {
+            leftsum += a[i - 1];
+            if(2*leftsum == sum - a[i])
+                return i + 1;
+        }
+        
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N; 
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.equalSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
