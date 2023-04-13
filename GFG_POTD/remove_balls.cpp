2eef6790e2736d0b6d084847539c99cc4b6eef7d
Om Patil <kushpo.030507@gmail.com>
2023-04-14 01:33:02 +0530

    Remove cons ele with same color and radius and give arr size

---
+++ GFG_POTD/remove_balls.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int finLength(int n, vector<int> color, vector<int> radius) {
+        
+        stack<int> st;
+        st.push(0);
+        int i = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(!st.empty() && color[i] == color[st.top()] && radius[i] == radius[st.top()])
+                st.pop();
+            else st.push(i);
+        }
+        
+        return st.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+
+        int n;
+        cin >> n;
+        vector<int> color(n), radius(n);
+        for (int i = 0; i < n; i++) cin >> color[i];
+        for (int i = 0; i < n; i++) cin >> radius[i];
+        int ans = sol.finLength(n, color, radius);
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
