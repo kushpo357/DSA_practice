1b283580438be40f25f07bd05f2fbe1dd3131687
Om Patil <kushpo.030507@gmail.com>
2023-05-06 22:37:48 +0530

    Find if it is possible to use exactly k coins to get target value

---
+++ GFG_POTD/coin_change_prob.cpp
@@ -0,0 +1,18 @@
+bool makeChanges(int N, int K, int target, vector<int> &coins) {
+    // code here
+    bool dp[K+1][target+1];
+    for(int j = 0; j <= K; j++) {
+        for(int k = 0; k <= target; k++) {
+            dp[j][k] = false;
+        }
+    }
+    dp[0][0] = true;
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= K; j++) {
+            for(int k = coins[i-1]; k <= target; k++) {
+                dp[j][k] |= dp[j-1][k-coins[i-1]];
+            }
+        }
+    }
+    return dp[K][target];
+}
