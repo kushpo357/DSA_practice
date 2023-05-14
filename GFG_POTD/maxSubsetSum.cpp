cdaf8f9a3b2de49a795422d81ba8fcbef7aa71b7
Om Patil <kushpo.030507@gmail.com>
2023-05-14 21:38:17 +0530

    Find the max subsetsum given atleast 1 of 2 cons ele must be considered

---
+++ GFG_POTD/maxSubsetSum.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class IntArray
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    long long findMaxSubsetSum(int n, vector<int> &a) {
+        // code here
+        vector<long long> prev(2), curr(2);
+        prev[0] = 0;
+        prev[1] = a[0]; 
+        for (int i = 1; i < n; i++)
+        {
+            curr[1] = a[i] + max(prev[0], prev[1]);
+            curr[0] = prev[1];
+            prev = curr;
+        }
+        
+        return max(prev[0], prev[1]);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        IntArray::input(A,N);
+        
+        Solution obj;
+        long long res = obj.findMaxSubsetSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
