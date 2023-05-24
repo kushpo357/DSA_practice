4d23302629634deaa40b31dcaa6e8a34619a1cba
Om Patil <kushpo.030507@gmail.com>
2023-05-24 09:11:08 +0530

    Find the max num of bowls ; each bowl has equal amount of cookies

---
+++ GFG_POTD/max_identical_bowls.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMaximum(int n, vector<int> &arr) {
+        // code here
+        long long sum = 0;
+        for(int i = 0; i < n; i++) sum += arr[i];
+        
+        for(int i = n; i > 0; i--)
+            if(!(sum % i)) return i;
+            
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> arr(N);
+        Array::input(arr,N);
+        
+        Solution obj;
+        int res = obj.getMaximum(N, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
