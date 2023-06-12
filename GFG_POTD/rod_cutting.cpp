55368ddd061c485328171ff2d231e9ec774bdff6
Om Patil <kushpo.030507@gmail.com>
2023-06-12 23:31:40 +0530

    Find the max possible of cutting the rod and selling the pieces

---
+++ GFG_POTD/rod_cutting.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        //code here
+        
+        vector<int> dp(n + 1, 0);
+        
+        for(int i = 1; i <= n; i++)
+            for(int ind = 0; ind < i; ind++)
+                dp[i] = max(dp[i], dp[ind] + price[i-ind-1]);
+       
+        return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+            
+        Solution ob;
+
+        cout << ob.cutRod(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
