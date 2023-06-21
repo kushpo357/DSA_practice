14955f20d5b074ecc81e869cf4bbeb89a0873d82
Om Patil <kushpo.030507@gmail.com>
2023-06-21 22:30:49 +0530

    give the sum of first n natural numbers

---
+++ GFG_POTD/reverseCoding.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        long long mod = 1000000007;
+        long long ans = ((n % mod * (n + 1) % mod / 2)% mod)% mod;
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
