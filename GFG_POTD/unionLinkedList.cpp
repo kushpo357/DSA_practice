6666983f13163d554adcb2f1d0b175a38c9a200a
Om Patil <kushpo.030507@gmail.com>
2023-06-27 23:26:04 +0530

    Return the union of the 2 linked list in the sorted order

---
+++ GFG_POTD/unionLinkedList.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code here
+        set<int> s;
+        while(head1)
+        {
+            s.insert(head1->data);
+            head1 = head1->next;
+        }
+        while(head2)
+        {
+            s.insert(head2->data);
+            head2 = head2->next;
+        }
+        struct Node* root = new Node(*s.begin());
+        s.erase(s.begin());
+        struct Node* curr = root;
+        for(int i : s)
+        {
+            struct Node* temp = new Node(i);
+            curr->next = temp;
+            curr = temp;
+        }
+        return root;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
