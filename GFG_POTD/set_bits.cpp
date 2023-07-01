2a504d26785058ba39b10079c00ac0aaf3f68603
Om Patil <kushpo.030507@gmail.com>
2023-07-01 19:14:09 +0530

    find the num of set bits in the number

---
+++ GFG_POTD/set_bits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int count = 0;
+        while(n)
+        {
+            if(n&1) count++;
+            n = n>>1;
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
