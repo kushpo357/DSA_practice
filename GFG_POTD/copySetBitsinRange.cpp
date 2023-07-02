2db4daf8467af50bb6002263e22e4bacfe96893a
Om Patil <kushpo.030507@gmail.com>
2023-07-02 22:23:55 +0530

    copy a part of one nums binary rep into another nums binary rep

---
+++ GFG_POTD/copySetBitsinRange.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+    public:
+    int setSetBit(int x, int y, int l, int r){
+        // code here
+        queue<int> q;
+        y = y>>(l-1);
+        for(int i = l; i <= r; i++)
+        {
+            int bit = y&1;
+            y = y>>1;
+            x = x | (bit << (i - 1));
+        }
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y, l, r;
+        cin>>x>>y>>l>>r;
+        
+        Solution ob;
+        cout<<ob.setSetBit(x, y, l, r)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
