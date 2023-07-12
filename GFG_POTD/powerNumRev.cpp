78efa041486f1561c18b03de7a48230af07c2a19
Om Patil <kushpo.030507@gmail.com>
2023-07-12 21:10:09 +0530

    Find the num raised to the power of its reverse

---
+++ GFG_POTD/powerNumRev.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    //You need to complete this fucntion
+    #define MOD 1000000007
+    long long power(int n,int r)
+    {
+       //Your code here
+        
+        if(!r) return 1;
+        
+        long long temp = power(n, r/2);
+        
+        if(r % 2) return n*(temp*temp % MOD) % MOD;
+        return temp*temp % MOD;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+// compute reverse of a number 
+long long rev(long long n)
+{
+    long long rev_num = 0;
+     while(n > 0) 
+      { 
+        rev_num = rev_num*10 + n%10; 
+        n = n/10; 
+      } 
+      return rev_num;
+}
+
+
+
+
+int main()
+{
+    int T;
+    cin>>T;//testcases
+    
+    while(T--)
+    {
+        long long N;
+        cin>>N;//input N
+        
+        long long R = 0; 
+        
+        // reverse the given number n
+        R = rev(N);
+        Solution ob;
+        //power of the number to it's reverse
+        long long ans =ob.power(N,R);
+        cout << ans<<endl;
+    }
+}
+// } Driver Code Ends
