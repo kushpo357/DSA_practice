65f7be62ff9d803c6244af5406673ad92f46f578
Om Patil <kushpo.030507@gmail.com>
2023-07-13 20:04:34 +0530

    Find if the no. occurence of all no. in the array is distinct or not

---
+++ GFG_POTD/uniqueNoOccur.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {
+        map<int, int> mp;
+        
+        for(int i = 0; i < n; i++) mp[arr[i]]++;
+        
+        map<int, int> mp2;
+        
+        for(auto i: mp)
+        {
+            mp2[i.second]++;
+            if(mp2[i.second] > 1) return false;
+        }
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
