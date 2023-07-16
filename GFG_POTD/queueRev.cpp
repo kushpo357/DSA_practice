51d3278040eb7901cb1f1ff37763a21a6410167a
Om Patil <kushpo.030507@gmail.com>
2023-07-16 16:19:44 +0530

    Reverse the given queue

---
+++ GFG_POTD/queueRev.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        int qn = q.size();
+        vector<int> v(qn);
+        
+        for(int i = 0; i < qn; i++)
+        {
+            v[i] = q.front();
+            q.pop();
+        }
+        
+        reverse(v.begin(), v.end());
+        
+        for(int i = 0; i < qn; i++) q.push(v[i]);
+        
+        return q;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
