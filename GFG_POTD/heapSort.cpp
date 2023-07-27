efd0f69237f79d949d29bdc3f2c5433fd3bbeedc
Om Patil <kushpo.030507@gmail.com>
2023-07-27 22:19:04 +0530

    Write the function for heapsort

---
+++ GFG_POTD/heapSort.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+// C++ program for implementation of Heap Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// The functions should be written in a way that array become sorted 
+// in increasing order when heapSort() is called.
+
+class Solution
+{
+    public:
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)  
+    {
+      // Your Code Here
+        int largest = i;
+    
+    
+        int l = 2 * i + 1;
+        
+        int r = 2 * i + 2;
+        
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+            
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+    
+        if (largest != i) 
+        {
+            swap(arr[i], arr[largest]);
+            heapify(arr, n, largest);
+        }
+    }
+
+    public:
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)  
+    { 
+    // Your Code Here
+     for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+    }
+
+    
+    public:
+    //Function to sort an array using Heap Sort.
+    void heapSort(int arr[], int n)
+    {
+        //code here
+        buildHeap(arr, n);
+        
+        for(int i = n - 1; i >= 0; i--)
+        {
+            swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+        }
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    Solution ob;
+    ob.heapSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
