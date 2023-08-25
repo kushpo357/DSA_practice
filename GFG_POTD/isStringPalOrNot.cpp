353f9616b2c076e0cb82c696fa30cefd3cadcfb6
Om Patil <kushpo.030507@gmail.com>
2023-08-25 08:39:53 +0530

    Find if the string is palindrome or not

---
+++ GFG_POTD/isStringPalOrNot.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string s)
+	{
+	    // Your code goes here
+	    
+	    int n = s.size();
+	    
+	    for(int i = 0, j = n - 1; i < j; i++, j--)
+	    {
+	        if(s[i] != s[j]) return 0;
+	    }
+	    
+	    
+	    return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
