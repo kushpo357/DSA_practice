6e3a1dae37a18860bfd6163378d0d344deae02f4
Om Patil <kushpo.030507@gmail.com>
2023-08-27 10:33:47 +0530

    Reverse the given string

---
+++ GFG_POTD/revstring.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        int n = str.size();
+        for(int i = 0; i < n/2; i++) 
+            str[i] ^= str[n - i - 1] ^= str[i] ^= str[n - i - 1];
+        
+        return str;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
