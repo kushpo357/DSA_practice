08bebdfc1b3962c570e217c05130425a7db28c0f
Om Patil <kushpo.030507@gmail.com>
2023-08-29 09:41:25 +0530

    Delete all nodes from linked list with a val > itself on the right side

---
+++ GFG_POTD/deleteNodeGreaterVal.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    private: 
+    int largest;
+
+    public:
+    
+    Node *compute(Node *head)
+    {
+        // your code goes here
+        
+        if(head->next == NULL)
+        {
+            largest = head->data;
+            return head;
+        }
+        
+        head->next = compute(head->next);
+        
+        if(largest > head->data) return head->next;
+        else largest = head->data;
+        return head;
+    }
+    
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
