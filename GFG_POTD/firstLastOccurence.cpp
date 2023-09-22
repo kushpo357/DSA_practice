2335038c9b30accbc290ef97e51f313e96a56d24
Om Patil <kushpo.030507@gmail.com>
2023-09-22 09:35:52 +0530

    Give the first and the last occurence of a num in the sorted array

---
+++ GFG_POTD/firstLastOccurence.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    vector<int> find(int arr[], int n , int x )
+    {
+        // code here
+        
+        auto first = lower_bound(arr, arr + n, x);
+        if(*first != x) return{-1, -1};
+        auto last = upper_bound(arr, arr + n, x);
+        last--;
+        return {first - arr, last - arr};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        Solution ob;
+        ans=ob.find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
