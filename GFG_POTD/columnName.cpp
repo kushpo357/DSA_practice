a4ce76d5eb958b5b9308f53688f49531a09400b9
Om Patil <kushpo.030507@gmail.com>
2023-10-03 13:09:42 +0530

    no 1 = A 2 = B .. 27 = AA in such way give the column name for the given num

---
+++ GFG_POTD/columnName.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        
+        string ans = "";
+        
+        while(n)
+        {
+            ans.push_back(--n % 26 + 'A');
+            n /= 26;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+
+// } Driver Code Ends
