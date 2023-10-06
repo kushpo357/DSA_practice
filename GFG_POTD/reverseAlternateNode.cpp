ae3e122e4ab8186be0501776fa80bba7910d472a
Om Patil <kushpo.030507@gmail.com>
2023-10-06 16:23:09 +0530

    Append every second node of the list at the end with last one coming first

---
+++ GFG_POTD/reverseAlternateNode.cpp
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    void rearrange(struct Node *odd)
+    {
+        //add code here
+        
+        if(odd->next == NULL) return;
+        
+        struct Node * append = odd->next;
+        
+        odd->next = odd->next->next;
+        
+        struct Node * curr = odd->next;
+        
+        append->next = NULL;
+        
+        while(curr != NULL && curr->next != NULL)
+        {
+            struct Node* temp = curr->next->next;
+            curr->next->next = append;
+            append = curr->next;
+            curr->next = temp;
+            curr = curr->next;
+        }
+        
+        curr = odd->next;
+        
+        while(curr->next) curr = curr->next;
+        
+        curr->next = append;
+        
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
