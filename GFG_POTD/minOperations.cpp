ef6d9cb7d6af9371e1fb988ab91471dd6148babd
Om Patil <kushpo.030507@gmail.com>
2023-10-26 00:11:47 +0530

    Find min no. of operations needed for t : 0 -> n by either t++ or t *= 2

---
+++ GFG_POTD/minOperations.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        int ans = 0;
+        
+        while(n)
+        {
+            if(n & 1)
+            {
+                n--;
+            }
+            else n /= 2;
+            
+            ans++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
