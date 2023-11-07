df0a025d2f37b2ae3b0e8d61ec3e623754ebe221
Om Patil <kushpo.030507@gmail.com>
2023-11-07 13:36:11 +0530

    Given a square matrix return the sum of upper and lower triangles

---
+++ GFG_POTD/SumUpperLowerTriangle.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to return sum of upper and lower triangles of a matrix.
+    vector<int> sumTriangles(const vector<vector<int> >& mat, int n)
+    {
+        // code here
+        
+        vector<int> ans(2, 0);
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i <= j) ans[0] += mat[i][j];
+                if(i >= j) ans[1] += mat[i][j];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        Solution ob;
+        vector<int> result = ob.sumTriangles(matrix,n);
+        for (int i = 0; i < result.size(); ++i)
+            cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
