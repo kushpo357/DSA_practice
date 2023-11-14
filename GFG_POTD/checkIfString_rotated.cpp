32e5c037f24ae6a34c8b84c3dbdce008e04dbe9d
Om Patil <kushpo.030507@gmail.com>
2023-11-14 23:09:08 +0530

    Check is str2 is rotated version of str1 or not

---
+++ GFG_POTD/checkIfString_rotated.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        
+        if(s1.size() != s2.size())
+            return false;
+        s2.append(s2);
+        
+        if(s2.find(s1) == string :: npos)
+            return false;
+            
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
