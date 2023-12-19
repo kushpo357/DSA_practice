6d77747390e8679aa244c1e73ea56ac9e534089f
Om Patil <kushpo.030507@gmail.com>
2023-12-19 23:41:32 +0530

    Find the rightmost bit position which is different in both files

---
+++ GFG_POTD/rightmostDiffBit.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here
+        int count = 0;
+        while(m || n)
+        {
+            count++;
+            if((m & 1) != (n & 1))
+              return count;
+              
+            n = n >> 1;
+            m = m >> 1;
+        }
+        
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
