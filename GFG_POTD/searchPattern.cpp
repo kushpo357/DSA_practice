66a06cd14ef0d2f98bec7e87c2c43f4ce2f60131
Om Patil <kushpo.030507@gmail.com>
2024-01-09 00:14:53 +0530

    Find all the occurences of the pat string in the txt string

---
+++ GFG_POTD/searchPattern.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            //code hee.
+            vector<int> ans;
+            
+            size_t temp = txt.find(pat);
+            while(temp != string::npos)
+            {
+                ans.push_back(temp + 1);
+                temp = txt.find(pat, temp + 1);
+            }
+            
+            return ans;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
