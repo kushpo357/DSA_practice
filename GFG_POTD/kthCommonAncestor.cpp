45d2e213d2076029d241cd8286f6d2e6c4e2797e
Om Patil <kushpo.030507@gmail.com>
2024-04-04 01:29:12 +0530

    Find the kth common ancestor for given x and y

---
+++ GFG_POTD/kthCommonAncestor.cpp
@@ -0,0 +1,177 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// A Tree node
+struct Node
+{
+	int data;
+	struct Node *left, *right;
+};
+
+// Utility function to create a new node
+Node* newNode(int data)
+{
+	Node* temp = new Node;
+	temp->data = data;
+	temp->left = temp->right = NULL;
+	return (temp);
+}
+
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*// A Tree node
+struct Node
+{
+	int data;
+	struct Node *left, *right;
+};*/
+
+
+class Solution
+{
+    public:
+    
+    /*You are required to complete below function */
+    
+    void solve(Node* root,const int &x,const int &y, vector<int> &ancestor)
+    {
+        if(root == NULL) return;
+        
+        if(x <= root->data && y >= root->data)
+        {
+            ancestor.push_back(root->data);
+            return;
+        }
+        
+        if(x <= root->data && y <= root->data)
+        {
+            solve(root->left, x, y, ancestor);
+        }
+        else
+        {
+           solve(root->right, x, y, ancestor);
+        }
+        
+        ancestor.push_back(root->data);
+    }
+    int kthCommonAncestor(Node *root, int k,int x, int y)
+    {
+        // your code goes here
+        vector<int> ancestor;
+        
+        if(x > y) swap(x, y);
+        
+        solve(root, x, y, ancestor);
+        
+        int n = ancestor.size();
+        if(n < k) return -1;
+        
+        // for(int i = 0; i < n; i++)
+        // {
+        //     cout<<ancestor[i]<<endl;
+        // }
+        
+        return ancestor[k - 1];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main ()
+{
+
+	int t;
+	cin >> t;
+	cin.ignore();
+
+	while (t--)
+	{
+	
+		string inp;
+		getline(cin, inp);
+		
+		int k,x,y;
+		cin>>k>>x>>y;
+		cin.ignore();
+
+		struct Node* root = buildTree(inp);
+
+        Solution ob;
+		int ans = ob.kthCommonAncestor(root,k,x,y);
+		cout<<ans<<endl;
+
+	}
+
+	return 0;
+}
+// } Driver Code Ends
