12179ea49d00d2130ab5e8a4234831e593eb92bb
Om Patil <kushpo.030507@gmail.com>
2024-04-20 08:30:45 +0530

    Find union of two sorted arrays

---
+++ GFG_POTD/sorted_arrays_union.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        //Your code here
+        vector<int> ans;
+        
+        ans.push_back(min(arr1[0], arr2[0]));
+        int i = 0, j = 0;
+        while(i < n && j < m)
+        {
+            if(ans.back() != min(arr1[i], arr2[j]))
+            {
+                ans.push_back(min(arr1[i], arr2[j]));
+            }
+            if(arr1[i] == arr2[j])
+            {
+                i++;
+                j++;
+            }
+            else if(arr1[i] > arr2[j]) j++;
+            else i++;
+        }
+        
+        while(i < n)
+        {
+            if(arr1[i] > ans.back())
+            ans.push_back(arr1[i]);
+            i++;
+        }
+        while(j < m)
+        {
+            if(arr2[j] > ans.back())
+            ans.push_back(arr2[j]);
+            j++;
+        }
+        //return vector with correct order of elements
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
