a7f46167edb5052043e67ad17aae0970ab60060f
Om Patil <kushpo.030507@gmail.com>
2024-05-18 00:08:08 +0530

    Find highest element in strictly increasing/decreasing array

---
+++ GFG_POTD/ind_highest_element.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        
+        int n = a.size();
+        int ans = a[0];
+        
+        for(int i = 1; i < n && a[i] > a[i - 1]; i++)
+        {
+            ans = a[i];
+        }
+        
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
