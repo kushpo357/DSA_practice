97e77d8cbb77fd66c0f4a8047f56aa45fc42b559
Om Patil <kushpo.030507@gmail.com>
2024-05-19 00:40:08 +0530

    Find closest value to k in sorted array

---
+++ GFG_POTD/find_closest_value.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int findClosest( int n, int k,int arr[]) 
+    { 
+        // Complete the function
+        
+        auto it = lower_bound(arr, arr + n, k);
+        
+        int ind = (int)(it - arr);
+        if(ind)
+        {
+            if(abs(arr[ind] - k) > abs(arr[ind - 1] - k))
+                ind--;
+        }
+        
+        return arr[ind];
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.findClosest(n,k,arr)<<endl;
+    }
+}
+
+// } Driver Code Ends
