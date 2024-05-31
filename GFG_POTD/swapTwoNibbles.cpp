766a5e4ffdae308db34b8afdd72e37ac31d2bb11
Om Patil <kushpo.030507@gmail.com>
2024-05-31 16:22:59 +0530

    Swap the two nibbles of a given number

---
+++ GFG_POTD/swapTwoNibbles.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        // code here
+        int nr = 15, nl = 240;
+        nr = nr & n;
+        nl = nl & n;
+        nr = nr << 4;
+        nl = nl >> 4;
+        
+        return nl + nr;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
