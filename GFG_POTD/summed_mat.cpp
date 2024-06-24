65f6c1bf5bc7dcb2ca11b78377f5007892ac6732
Om Patil <kushpo.030507@gmail.com>
2024-06-24 23:40:55 +0530

    Count cells with value q in n*n matrix where M(i,j) = i+j

---
+++ GFG_POTD/summed_mat.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        // code here
+        
+        if(2*n < q) return 0;
+        
+        if(q <= n) return q - 1;
+        
+        return (2*n - q) + 1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
