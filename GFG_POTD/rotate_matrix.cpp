01380c8dbbe65386c9e7166c5107c20904053a4e
Om Patil <kushpo.030507@gmail.com>
2024-06-25 19:14:40 +0530

    Rotate elements of matrix to the left k times and return result

---
+++ GFG_POTD/rotate_matrix.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> rotateMatrix(int k, vector<vector<int>> mat) {
+        // code here
+        
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        k = k % m;
+        
+        for(int i = 0; i < n; i++)
+        {
+            vector<int> v(m, 0);
+            for(int j = 0; j < m; j++)
+            {
+                v[(m  + j - k)%m] = mat[i][j];
+            }
+            
+            mat[i] = v;
+        }
+        
+        return mat;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<vector<int>> mat(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> mat[i][j];
+        Solution ob;
+        vector<vector<int>> ans = ob.rotateMatrix(k, mat);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cout << ans[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+
+// } Driver Code Ends
