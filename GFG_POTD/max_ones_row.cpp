e80284a6b0256421812b4133f4f1f06b8a6799ba
Om Patil <kushpo.030507@gmail.com>
2024-07-29 21:51:08 +0530

    Find the row with the maximum number of 1s in a sorted 2D boolean array

---
+++ GFG_POTD/max_ones_row.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        // code here
+        
+        int n = arr.size();
+        int m = arr[0].size();
+        
+        int ans = 0;
+        
+        int i = 0, j = m - 1;
+        while(i < n && j >= 0)
+        {
+            if(arr[i][j] == 0)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+                ans = i;
+            }
+        }
+        
+        if(j == m - 1)
+            return -1;
+            
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
