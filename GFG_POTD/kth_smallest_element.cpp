1120476cf277e656fcaafc2e46aa4357e7f644fd
Om Patil <kushpo.030507@gmail.com>
2024-08-19 23:56:00 +0530

    Find the kth smallest element in an array where all elements are distinct

---
+++ GFG_POTD/kth_smallest_element.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> arr, int k) {
+        // code here
+        
+        set<int> s;
+        
+        for(int i = 0; i < k; i++)
+        {
+            s.insert(arr[i]);
+        }
+        
+        int n = arr.size();
+        for(int i = k; i < n; i++)
+        {
+            s.insert(arr[i]);
+            s.erase(--s.end());
+        }
+        
+        return *(--s.end());
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
+        Solution ob;
+        cout << ob.kthSmallest(arr, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
