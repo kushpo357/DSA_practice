03572e65bdbace54adc0630b143428617cc7c327
Om Patil <kushpo.030507@gmail.com>
2024-09-07 11:41:03 +0530

    Implement function to find the nth natural number excluding numbers containing the digit 9.

---
+++ GFG_POTD/find_natural_number_excluding_9.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long findNth(long long n) {
+        // code here.
+        
+        long long ans = 0;
+        long long num = 1;
+        
+        while(n)
+        {
+            ans += (n % 9)*num;
+            n = n / 9;
+            num = num*10;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, ans;
+        cin >> n;
+        Solution obj;
+        ans = obj.findNth(n);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
