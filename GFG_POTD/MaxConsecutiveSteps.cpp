f209f1aee67022f08b665ebfc50601d9002b7132
Om Patil <kushpo.030507@gmail.com>
2024-09-26 23:31:08 +0530

    Added C++ solution to find the maximum consecutive steps with altitude gain

---
+++ GFG_POTD/MaxConsecutiveSteps.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    int maxStep(vector<int>& arr) {
+        // Your code here
+        
+        int ans = 0;
+        int curr = 0;
+        for(int i = 1; i < arr.size(); i++)
+        {
+            if(arr[i] > arr[i - 1])
+            {
+                curr++;
+                ans = max(ans, curr);
+            }
+            else
+            {
+                curr = 0;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxStep(arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
